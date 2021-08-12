d80cacd0c0ae479c79528a5c53135da10df7b164
Ulises Velazquez Juarez <hatachi47@gmail.com>
2021-08-12 13:39:53 -0500

    Fin Seccion 3

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+//App de Express
+
+//Node Server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+
+//Mensajes de Sockets
+require('./sockets/socket');
+
+//Path publico
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+  if (err) throw new Error(err);
+  console.log("Servidor corriendo en puerto!!!!", process.env.PORT);
+});
