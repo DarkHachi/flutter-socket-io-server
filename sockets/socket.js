f6a4df5e99e46e9432ec1149f7689927ab8b768d
Ulises Velazquez Juarez <hatachi47@gmail.com>
2021-08-13 13:00:03 -0500

    Modificacion en el trabajo - Seccion Incompleta

--- sockets/socket.js
+++ sockets/socket.js
@@ -11,4 +11,10 @@ io.on("connection", (client) => {
     console.log("Mensaje!!!", payload);
     io.emit("mensaje", { admin: "Nuevo mensaje" });
   });
+
+  client.on("nuevo-mensaje", (payload) => {
+    console.log(payload);
+    // io.emit("nuevo-mensaje", payload); //emite el mensaje a todos
+    client.broadcast.emit("nuevo-mensaje", payload); //emite a todos menos a quien emitio
+  });
 });
