3953d687ff88280fcd5053f3eddb67654802a010
DarkHachi <hatachi47@gmail.com>
2021-08-19 00:40:06 -0500

    Modelo de Banda Agregado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,23 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metalica'));
+
+// console.log(bands);
+
 
 io.on("connection", (client) => {
   console.log("Cliente Conectado");
 
+  client.emit('active-bands',bands.getBands());
+  
+  
   client.on("disconnect", () => {
     console.log("Cliente Desconectado");
   });
@@ -11,10 +26,15 @@ io.on("connection", (client) => {
     console.log("Mensaje!!!", payload);
     io.emit("mensaje", { admin: "Nuevo mensaje" });
   });
-
+  
   client.on("nuevo-mensaje", (payload) => {
     console.log(payload);
     // io.emit("nuevo-mensaje", payload); //emite el mensaje a todos
     client.broadcast.emit("nuevo-mensaje", payload); //emite a todos menos a quien emitio
+  });
+  
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands',bands.getBands());
   });
 });
